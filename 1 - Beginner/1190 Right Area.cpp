66322a12a51ff7668698b619c371049014022cf6
Lucas Alves <lucas.a.freitas16@gmail.com>
2022-12-29 23:41:32 -0300

    overcomplicated solution

---
+++ 1 - Beginner/1190 Right Area.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    char operation{};
+    std::cin >> operation;
+
+    constexpr int size{ 12 };
+    double numbers[size][size]{};
+    for(int row = 0; row < size; ++row) {
+        for(int col = 0; col < size; ++col) {
+            std::cin >> numbers[row][col];
+        }
+    }
+
+    double result{};
+    for (int row = 0; row < size; ++row) {
+        int start = (row < 6 ? size - row : size - (size - row) + 1);
+        for (int col = start; col < size; ++col) {
+            result += numbers[row][col];
+        }
+    }
+
+    if (operation == 'M') {
+        // adaptation of N(N+1)/2 to get the sum
+        // of 1 to N numbers!
+        int half{ (size - 2) / 2};
+        int counter{ half * (half + 1) };
+        result /= counter;   // calculates average
+    }
+    std::cout << std::setprecision(1) << std::fixed;
+    std::cout << result << '\n';
+    return 0;
+}
