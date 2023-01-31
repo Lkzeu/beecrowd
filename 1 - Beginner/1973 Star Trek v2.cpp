4ca88790f1843920d85b9e1ae96d748e3c018178
Lucas Alves <lucas.a.freitas16@gmail.com>
2023-01-31 13:25:51 -0300

    No heap allocations, no arrays, just one loop.
    When he attack a even number of sheep in a star, he starts to
    go to star i - 1. Since he already stole 1 from it, it now has an even
    number of sheeps that will made him go back to the beggining and halts
    his trek.
    If a star had 1 sheep the first time he passed by it, if he starts the
    "going back jorney" it will have 0, so he'll stole none. Because of it
    I store each zero cases so it will be removed from the arithmetic operation
    that gives us the number of stolen sheeps.
    If a star has a even number a sheeps he stole 1 sheep from this star and
    start to fall back, so he'll stole just 1 sheep from it. Because of it I
    remove 1 in the formula.

---
+++ 1 - Beginner/1973 Star Trek v2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+    int number_of_stars{};
+    std::cin >> number_of_stars;
+
+    long long non_stolen_sheeps{ 0 };
+    bool even_attacked{ false };
+    int pos_even_attacked{};
+    int zeros{ 0 };
+    for (int i = 0; i < number_of_stars; ++i)
+    {
+        int temp{};
+        scanf("%d", &temp);
+        non_stolen_sheeps += temp;
+        if (!even_attacked && temp == 1) { ++zeros; }
+
+        if (!even_attacked && temp % 2 == 0) {
+            even_attacked = true;
+            pos_even_attacked = i + 1;
+        }
+    }
+    
+    long long takeoff  { number_of_stars };
+    int  stars_attacked{ pos_even_attacked ? pos_even_attacked : number_of_stars };
+    if (even_attacked) {
+        takeoff = (stars_attacked * 2) - zeros - 1; 
+    }
+
+    non_stolen_sheeps -= takeoff;
+    std::cout << stars_attacked << " " << non_stolen_sheeps << '\n';
+    return 0;
+}
