cc20e841c8e2078efdbc2759de9b9e34323a21e8
Lucas Alves <lucas.a.freitas16@gmail.com>
2023-02-02 12:37:15 -0300

    more readable and cohesive

--- 1 - Beginner/1973 Star Trek v2.cpp
+++ 1 - Beginner/1973 Star Trek v2.cpp
@@ -6,9 +6,11 @@ int main()
     int number_of_stars{};
     std::cin >> number_of_stars;
 
+    bool even_attacked { false };
+    int stars_attacked { number_of_stars };
+    long long take_away{ number_of_stars };
+
     long long non_stolen_sheeps{ 0 };
-    bool even_attacked{ false };
-    int pos_even_attacked{};
     int zeros{ 0 };
     for (int i = 0; i < number_of_stars; ++i)
     {
@@ -19,16 +21,10 @@ int main()
 
         if (!even_attacked && temp % 2 == 0) {
             even_attacked = true;
-            pos_even_attacked = i + 1;
+            stars_attacked = i + 1;
+            take_away = (stars_attacked * 2) - zeros - 1; 
         }
     }
-    
-    long long take_away  { number_of_stars };
-    int  stars_attacked{ pos_even_attacked ? pos_even_attacked : number_of_stars };
-    if (even_attacked) {
-        take_away = (stars_attacked * 2) - zeros - 1; 
-    }
-
     non_stolen_sheeps -= take_away;
     std::cout << stars_attacked << " " << non_stolen_sheeps << '\n';
     return 0;
